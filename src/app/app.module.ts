7671d82121f70cf4313d0c3f51615d01b98b48ce
8aChristian <145133332+8aChristian@users.noreply.github.com>
2024-03-29 17:31:47 -0500

    Final

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,10 +6,13 @@ import { IonicModule, IonicRouteStrategy } from '@ionic/angular';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { initializeApp, provideFirebaseApp } from '@angular/fire/app';
+import { getFirestore, provideFirestore } from '@angular/fire/firestore';
+import { getDatabase, provideDatabase } from '@angular/fire/database';
 
 @NgModule({
   declarations: [AppComponent],
-  imports: [BrowserModule, IonicModule.forRoot(), AppRoutingModule],
+  imports: [BrowserModule, IonicModule.forRoot(), AppRoutingModule, provideFirebaseApp(() => initializeApp({"projectId":"rtdb-iot-5309b","appId":"1:661199907590:web:23cb19d154b7a59a6c0c87","databaseURL":"https://rtdb-iot-5309b-default-rtdb.firebaseio.com","storageBucket":"rtdb-iot-5309b.appspot.com","apiKey":"AIzaSyAOyhjEsYSNnOWnUic9QMUlmK5mhrCKu_g","authDomain":"rtdb-iot-5309b.firebaseapp.com","messagingSenderId":"661199907590"})), provideFirestore(() => getFirestore()), provideDatabase(() => getDatabase())],
   providers: [{ provide: RouteReuseStrategy, useClass: IonicRouteStrategy }],
   bootstrap: [AppComponent],
 })
--- src/app/tab1/tab1.page.ts
+++ src/app/tab1/tab1.page.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Database, object, ref, set } from '@angular/fire/database'; // Importa el método set para actualizar la base de datos
 
 @Component({
   selector: 'app-tab1',
@@ -6,7 +7,27 @@ import { Component } from '@angular/core';
   styleUrls: ['tab1.page.scss']
 })
 export class Tab1Page {
+  espacios: { nombre: string, estado: boolean }[] = []; // Declaración explícita del tipo de espacios
 
-  constructor() {}
+  constructor(private database: Database) {}
 
+  ngOnInit() {
+    const route = ref(this.database, 'casa');
+    object(route).subscribe(attributes => {
+      this.espacios = [
+        { nombre: 'Baño', estado: attributes.snapshot.val().Baño },
+        { nombre: 'Cocina', estado: attributes.snapshot.val().Cocina },
+        { nombre: 'Comedor', estado: attributes.snapshot.val().Comedor },
+        { nombre: 'Dormitorio', estado: attributes.snapshot.val().Dormitorio },
+        { nombre: 'Estudio', estado: attributes.snapshot.val().Estudio },
+        { nombre: 'Sala', estado: attributes.snapshot.val().Sala }
+      ];
+    });
+  }
+
+  toggleLight(espacio: { nombre: string, estado: boolean }) {
+    espacio.estado = !espacio.estado;
+    const route = ref(this.database, 'casa/' + espacio.nombre); // Ruta específica para el espacio actual
+    set(route, espacio.estado); // Actualiza el estado en la base de datos
+  }
 }
